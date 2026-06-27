4191dcea30603c352a92516b7ba594e5ec8c6955
dhhi0101 <dohee050101@gmail.com>
2026-06-27 22:44:08 +0900

    fix: force IPv4 DNS resolution for Render free tier

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require("dns").setDefaultResultOrder("ipv4first");
+
 const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
